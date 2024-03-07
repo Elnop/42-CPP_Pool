3bd67cfdd09085bd0f8bb2d3e02057deb629f8a9
elnop <elnop@lp-ubuntu.dev>
2024-03-07 20:37:59 +0100

    remove useless lines

--- Module_06/ex01/main.cpp
+++ Module_06/ex01/main.cpp
@@ -3,8 +3,6 @@
 #include <iostream>
 int main(void)
 {
-	// Serialize serialize;
-	// (void)serialize;
 	struct Data data = { 42, 8 };
 	uintptr_t data_ptr = Serialize::serialize(&data);
 	std::cout << "Data pointer: " << data_ptr << std::endl;
